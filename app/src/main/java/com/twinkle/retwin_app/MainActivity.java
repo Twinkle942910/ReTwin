d1d4be2d263b58b76b3f9a22cc18b83b187a7150
Twinkle942910 <akarpun94@gmail.com>
2016-02-29 23:51:57 +0200

    Adding Main activity and main layout

---
+++ app/src/main/java/com/twinkle/retwin_app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.twinkle.retwin_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Main Activity of App
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
