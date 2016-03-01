fbad33eb97223187a2f217b2f1935eccbab94a99
Twinkle942910 <akarpun94@gmail.com>
2016-03-02 00:30:53 +0200

    Old Android theme (Holo) without styles and manifest theme

--- app/src/main/java/com/twinkle/retwin_app/MainActivity.java
+++ app/src/main/java/com/twinkle/retwin_app/MainActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.app.AppCompatActivity;
  * Main Activity of App
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
