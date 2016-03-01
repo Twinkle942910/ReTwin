e5a1413f7cc3962177171739e6bbddda876726ff
Twinkle942910 <akarpun94@gmail.com>
2016-03-02 01:43:31 +0200

    Adding toolbar, own theme and menu with icon

--- app/src/main/java/com/twinkle/retwin_app/MainActivity.java
+++ app/src/main/java/com/twinkle/retwin_app/MainActivity.java
@@ -3,15 +3,36 @@ package com.twinkle.retwin_app;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Main Activity of App
  */
 
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
