21115f81c3687a7b378410ae517326e956b2cc0d
Twinkle942910 <akarpun94@gmail.com>
2016-03-03 01:14:50 +0200

    Adding items and header to navigation view, changing Main Activity

--- app/src/main/java/com/twinkle/retwin_app/MainActivity.java
+++ app/src/main/java/com/twinkle/retwin_app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.twinkle.retwin_app;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -11,15 +12,19 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.main_layout;
+
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView();
     }
 
     private void initToolbar() {
@@ -34,5 +39,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationView() {
+            drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 }
